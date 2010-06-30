96b4df609fd68c531d8754a053f53bf1b5ba779b
Michael Hafen <mdhafen@tech.washk12.org>
2010-06-30 12:24:01 -0600

    fixup first update to work
    The check of needing the update failed when it wasn't supposed to.  Changed the
    query and the comparison.  It doesn't fail when it isn't supposed to now, so
    the update will be applied.

--- updates/0001-add_course_order.php
+++ updates/0001-add_course_order.php
@@ -7,12 +7,12 @@ $db_settings = $config['database'];
 $table = $db_settings['schema'];
 
 $dbh = db_connect();
-$query = "SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '$table' AND TABLE_NAME = 'category' AND COLUMN_NAME = 'category_group_order'";
+$query = "SELECT COUNT(*) AS count FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '$table' AND TABLE_NAME = 'category' AND COLUMN_NAME = 'category_group_order'";
 $sth = $dbh->prepare( $query );
 $sth->execute();
 
 $row = $sth->fetch();
-if ( ! count( $row ) ) {
+if ( $row['count'] == 0 ) {
   $query = "ALTER TABLE category ADD COLUMN category_group_order INT(10) UNSIGNED AFTER category_group";
   return "Added table: category_group_order";
 }
