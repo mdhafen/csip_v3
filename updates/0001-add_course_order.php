0b45fc6b7ec9c385f04037a45957fbc2c292aa47
Michael Hafen <mdhafen@tech.washk12.org>
2010-06-30 12:28:31 -0600

    actually execute the query to update the database
    Missed that step.

--- updates/0001-add_course_order.php
+++ updates/0001-add_course_order.php
@@ -14,6 +14,8 @@ $sth->execute();
 $row = $sth->fetch();
 if ( $row['count'] == 0 ) {
   $query = "ALTER TABLE category ADD COLUMN category_group_order INT(10) UNSIGNED AFTER category_group";
+  $sth = $dbh->prepare( $query );
+  $sth->execute();
   return "Added column to category table: category_group_order";
 }
 
