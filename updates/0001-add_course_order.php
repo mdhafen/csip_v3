261c21b649b3362ef9d04b9bf3dc585bcc637579
Michael Hafen <mdhafen@tech.washk12.org>
2010-06-30 13:31:35 -0600

    change first database update-need two columns
    I decided that this would be best with two new columns instead of repurposing
    the category_group column.

--- updates/0001-add_course_order.php
+++ updates/0001-add_course_order.php
@@ -8,15 +8,14 @@ $table = $db_settings['schema'];
 
 $dbh = db_connect();
 $query = "SELECT COUNT(*) AS count FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '$table' AND TABLE_NAME = 'category' AND COLUMN_NAME = 'category_group_order'";
-$sth = $dbh->prepare( $query );
-$sth->execute();
-
-$row = $sth->fetch();
+$row = $dbh->query( $query );
 if ( $row['count'] == 0 ) {
-  $query = "ALTER TABLE category ADD COLUMN category_group_order INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER category_group";
-  $sth = $dbh->prepare( $query );
-  $sth->execute();
-  return "Added column to category table: category_group_order";
+  $query = "ALTER TABLE category ADD COLUMN course_group INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER category_group";
+  $dbh->exec( $query );
+
+  $query = "ALTER TABLE category ADD COLUMN course_group_order INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER course_group";
+  $dbh->exec( $query );
+  return "Added columns to category table: course_group, course_group_order";
 }
 
 ?>
