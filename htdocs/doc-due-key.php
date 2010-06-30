30ef7cee7b2f29d5d6f97c9dca1b75108a9dc9eb
Michael Hafen <mdhafen@tech.washk12.org>
2010-06-30 12:00:32 -0600

    show due dates as m/d/y
    add a couple function calls to the implicit echo's so the dates are formatted
    m/d/y instead of y-m-d.

--- htdocs/doc-due-key.php
+++ htdocs/doc-due-key.php
@@ -1,16 +1,16 @@
 <?php if ( $data['csip'] ) { ?>
 <div id="due_date_key">
 <?php if ( $data['csip']['sap_due_date'] ) { ?>
-SAP Goals Due: <?= $data['csip']['sap_due_date'] ?><br>
+SAP Goals Due: <?= date('m/d/Y', strtotime( $data['csip']['sap_due_date'] ) ) ?><br>
 <?php } ?>
 <?php if ( $data['csip']['csip_due_date'] ) { ?>
-CSIP Goals Due: <?= $data['csip']['csip_due_date'] ?><br>
+CSIP Goals Due: <?= date('m/d/Y', strtotime( $data['csip']['csip_due_date'] ) ) ?><br>
 <?php } ?>
 <?php if ( $data['csip']['board_due_date'] ) { ?>
-Community Council Review: <?= $data['csip']['board_due_date'] ?><br>
+Community Council Review: <?= date('m/d/Y', strtotime( $data['csip']['board_due_date'] ) ) ?><br>
 <?php } ?>
 <?php if ( $data['csip']['district_due_date'] ) { ?>
-Board Review: <?= $data['csip']['district_due_date'] ?><br>
+Board Review: <?= date('m/d/Y', strtotime( $data['csip']['district_due_date'] ) ) ?><br>
 <?php } ?>
 </div>
 <?php } ?>
