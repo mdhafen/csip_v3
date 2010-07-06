2a1e06bec5b2fed43560bb9cdeb1589f5b820307
Michael Hafen <mdhafen@tech.washk12.org>
2010-07-06 16:50:36 -0600

    wrong version number in a question
    The second question has a parser query that refers to the first question, but
    was using the wrong version in the query.

--- updates/0003-v3_questions.php
+++ updates/0003-v3_questions.php
@@ -20,7 +20,7 @@ VALUES
 [input_1]
 Students generally did not do as well on the following standards &amp; objectives:
 [input_1]' ),
- ( 3, 16, 2, 8, 'Which subgroup(s), with 10 or more members, had less than [[SELECT [answer_0] FROM answer CROSS JOIN question USING (questionid) WHERE csipid = [csipid] AND categoryid = [categoryid] AND part = 1 AND version = 2 AND order_num = 1]]% proficient?
+ ( 3, 16, 2, 8, 'Which subgroup(s), with 10 or more members, had less than [[SELECT [answer_0] FROM answer CROSS JOIN question USING (questionid) WHERE csipid = [csipid] AND categoryid = [categoryid] AND part = 1 AND version = 3 AND order_num = 1]]% proficient?
 <table><tbody><tr><td>[input_7] Asian</td><td>[input_7] Hispanic</td><td>[input_7] SPED</td></tr><tr><td>[input_7] Black</td><td>[input_7] Indian</td><td>[input_7] ELL</td></tr><tr><td>[input_7] Caucasian</td><td>[input_7] Pacific Islander</td><td>[input_7] Low Income</td></tr></tbody></table>' ),
 ";
   $result = $dbh->exec( $query );
