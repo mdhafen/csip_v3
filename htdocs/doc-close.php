84e6d498c1f6783835df74c153e0214c4d1a4b8a
Michael Hafen <mdhafen@tech.washk12.org>
2010-07-14 13:47:01 -0600

    changing the copyright to include current year

--- htdocs/doc-close.php
+++ htdocs/doc-close.php
@@ -1,7 +1,7 @@
 <div id="footer">
 <image class="bottom_left" src="<?= $data['_config']['base_url'] ?>images/bottom_left.png">
 <image class="bottom_right" src="<?= $data['_config']['base_url'] ?>images/bottom_right.png">
-The Digital Comprehensive School Improvement Plan (DCSIP) version 3, Copyright (C) 2009 WCSD
+The Digital Comprehensive School Improvement Plan (DCSIP) version 3, Copyright (C) 2009 - 2010 WCSD
 </div>
 </body>
 </html>
