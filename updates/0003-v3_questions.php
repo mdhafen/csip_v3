1f83927a854315e5a67d216daaa442bd829cde46
Michael Hafen <mdhafen@tech.washk12.org>
2010-07-20 12:58:19 -0600

    Fixing bug in display of checkbox answers and changing questions on part 1
    Brad had more changes to the version 3 questions, just part 1.
    Also noticed a bug while I was at it, and squashed it.

--- updates/0003-v3_questions.php
+++ updates/0003-v3_questions.php
@@ -16,18 +16,23 @@ INSERT INTO question (
  version, question_group, part, order_num, type, question_option_group, question
  )
 VALUES 
- ( 3, 16, 1, 1, 9, 0, 'Last year''s CRT scores for your current class(s) can be found on <a href=\"https://psa.washk12.org/teachers/\" target=\"_BLANK\">Power Teacher</a>:
+ ( 3, 16, 1, 1, 3, 0, 'Last year''s CRT scores for your current class(s) can be found on <a href=\"https://psa.washk12.org/teachers/\" target=\"_BLANK\">Power Teacher</a>:
 <ol><li style=\"padding: 0;\">From leftside menu select REPORTS.</li><li style=\"padding: 0;\">From list of WCSD Reports (bottom half of screen) select ASSESSMENT REPORTS</li><li style=\"padding: 0;\">Select current class.</li><li style=\"padding: 0;\">Choose a CRT and then choose ASSESSMENT SUMMART > Submit.</li></ol>
 
 From this report you can:
-<ol style=\"list-style-type: upper-alpha;\"><li style=\"padding: 0;\">See all testing information for your current students.</li><li style=\"padding: 0;\">All CRT scale scores below 160 are highlighted in pink</li><li style=\"padding: 0;\">Click on the scale score to drill down to Standard score.  Then click on the Standard to see the Objective score.</li><li style=\"padding: 0;\">Click on a student''s name to see an assessment summary.</li><li style=\"padding: 0;\">Subgroup information is summarized in the tables that follow the student list.  (Prof. is the number of Proficient students, Non. is the number of Non Proficient Students, Count is the total number of students in that subgroup.</li><li style=\"padding: 0;\">To export to an Excel file click DOWNLOAD CSV DATA</li></ol>' ),
- ( 3, 16, 1, 2, 8, 0, 'Overall [input_3]% of students were proficient on the [[SELECT category_name FROM category WHERE version = 3 AND course_group = [course_group] AND course_group_order = [course_group_order] -1]] CRT.  Students generally did well on the following standards &amp; objectives:
-[input_1]
-Students generally did not do as well on the following standards &amp; objectives:
-[input_1]
+<ol style=\"list-style-type: upper-alpha;\"><li style=\"padding: 0;\">See all testing information for your current students.</li><li style=\"padding: 0;\">All CRT scale scores below 160 are highlighted in pink</li><li style=\"padding: 0;\">Click on the scale score to drill down to Standard score.  Then click on the Standard to see the Objective score.</li><li style=\"padding: 0;\">Click on a student''s name to see an assessment summary.</li><li style=\"padding: 0;\">Subgroup information is summarized in the tables that follow the student list.  (Prof. is the number of Proficient students, Non. is the number of Non Proficient Students, Count is the total number of students in that subgroup.</li><li style=\"padding: 0;\">To export to an Excel file click DOWNLOAD CSV DATA</li></ol>
+Overall [input]% of students where proficient on the [[SELECT category_name FROM category WHERE version = 3 AND course_group = [course_group] AND course_group_order = [course_group_order] -1]] CRT.
 <input type=\"submit\" name=\"op\" value=\"Save Answers\">' ),
- ( 3, 16, 1, 3, 8, 0, 'Which subgroup(s), with 10 or more members, had less than [[SELECT [answer_0] FROM answer CROSS JOIN question USING (questionid) WHERE csipid = [csipid] AND categoryid = [categoryid] AND part = 1 AND version = 3 AND order_num = 2]]% proficient?
+ ( 3, 16, 1, 2, 8, 0, 'Subgroup(s), with 10 or more members, which had less than [[SELECT [answer_0] FROM answer CROSS JOIN question USING (questionid) WHERE csipid = [csipid] AND categoryid = [categoryid] AND part = 1 AND version = 3 AND order_num = 2]]% proficient?
 <table><tbody><tr><td>[input_7] Asian</td><td>[input_7] Hispanic</td><td>[input_7] SPED</td></tr><tr><td>[input_7] Black</td><td>[input_7] Indian</td><td>[input_7] ELL</td></tr><tr><td>[input_7] Caucasian</td><td>[input_7] Pacific Islander</td><td>[input_7] Low Income</td></tr></tbody></table>' ),
+ ( 3, 16, 1, 3, 1, 0, 'Additional data analysis can be conducted on the Utah State Office of Education <a href=\"https://cognos.schools.utah.gov/cognos8/cgi-bin/cognos.cgi?b_action=xts.run&m=portal/main.xts&startwel=yes\" target=\"_BLANK\">Data Display</a>
+
+<ol style=\"list-style-type: upper-alpha;\"><li>Userid = usoedd
+Password = cognos</li><li>Use the drop-down menus to view CRT results for any district, school, or year.</li><li>Use the drop-down menus to filter by demographic group and click SUBMIT.</li><li>The graphs and tables are drill-down links.</li><li>To see the numbers behind the percents click CRT BY NUMBER AND PERCENT at the bottom of the report.</li><li>CRT BY TEACHER displays class results by teacher Cactus number.</li></ol>
+Note: DATA DISPLAY is for educational users only.  DATA DISPLAY will not report individual students results.
+
+Below, enter any data analysis information the PLC found to be important (or as directed by the Principal)
+[input]' ),
  ( 3, 16, -1, 1, 9, 0, 'As you create target proficiency goals keep in mind the following Adequate Yearly Progress (AYP) targets:
 <table><tr><th>Subject / Grade</th><th>2011 &amp; 2012</th><th>2013</th><th>2014</th></tr><tbody><tr><td>Language Arts (3-8)</td><td class=\"text_center\">89%</td><td class=\"text_center\">95%</td><td class=\"text_center\">100%</td></tr><tr><td>Math (3-8)</td><td class=\"text_center\">63%</td><td class=\"text_center\">81%</td><td class=\"text_center\">100%</td></tr><tr><td>Language Arts (10)</td><td class=\"text_center\">88%</td><td class=\"text_center\">94%</td><td class=\"text_center\">100%</td></tr><tr><td>Math (10-12)</td><td class=\"text_center\">60%</td><td class=\"text_center\">80%</td><td class=\"text_center\">100%</td></tr></tbody></table>' ),
  ( 3, 16, -1, 2, 8, 0, 'At the beginning of the [year_name] we have
