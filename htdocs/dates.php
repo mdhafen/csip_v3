2980f56ca7f9a863ef71101279afe2d17160575d
Michael Hafen <michael.hafen@washk12.org>
2015-03-03 14:39:12 -0700

    hide guaranteed curriculum and completion dates until a csip is loaded
    the dates depends on the csip.
    the curriculum depends on the course.

--- htdocs/dates.php
+++ htdocs/dates.php
@@ -1,16 +1,16 @@
 
     <div class="uk-panel uk-panel-box">
+<?php if ( !empty($data['csip']['due_dates']) ) { ?>
         <h4><i class="uk-icon-calendar"></i> <strong>Expected Completion Dates</strong></h4>
 <hr>
     <ul>
         <font size="1pt">
 <?php
-if ( !empty($data['csip']['due_dates']) ) {
    $dates = explode( "\n", $data['csip']['due_dates'] );
    foreach ( $dates as $date ) {
       echo "<li>$date</li>";
    }
-}
 ?>
     </font></ul>
+<?php } ?>
 </div>
