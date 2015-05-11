02a6c2dd01d9e8860912da493841f1c861a6cc79
Michael Hafen <michael.hafen@washk12.org>
2015-05-11 13:38:00 -0600

    Add the wcsd 100 year logo to the footer

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,7 +1,10 @@
 <hr>
-    <div style="text-align: right; font-size: 9pt;">
+    <div class="clearfix">
+        <img src="https://logos.washk12.org/100year.png">
+        <div class="uk-align-right" style="font-size: 9pt;">
 		Developed for <strong>Washington County School District</strong> | A joint venture, powered by <img src="images/blackplum_logo_small.png" width="70" alt="BlackPlum Logo"> <i class="uk-icon-plus"></i> <strong>HAFEN</strong>
  &copy; 2015 Jim Black, Cody Plumhof and Michael Hafen.
-	</div>
+        </div>
+    </div>
 <br>
 <br>
