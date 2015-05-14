2fc8ca785c1c63bd3f512ebe2afbc098fc0c6d8a
Michael Hafen <michael.hafen@washk12.org>
2015-05-14 14:01:10 -0600

    Add Scient 4 and 5 Courses to the Migration script.

--- doc/DCSIP_v5_migrate.sql
+++ doc/DCSIP_v5_migrate.sql
@@ -3,6 +3,7 @@
 INSERT INTO csip_v5.course_categories ( categoryid, category_name ) VALUES (1,'Language Arts'),(2,'Math'),(3,'Science'),(4,'CTE'),(5,'Fine Arts'),(6,'Foreign Language'),(7,'Social Science'),(8,'Health Ed.'),(9,'Other');
 
 INSERT INTO csip_v5.course (courseid,course_name,course_category,active,min_grade,max_grade) (SELECT  categoryid,category_name,course_group,1,IF(gradelevel,gradelevel, CASE loc_cat_subcat WHEN 'ELEM' THEN 0 WHEN 'ELEM,INT,MID' THEN 0 WHEN 'INT' THEN 6 WHEN 'INT,MID,HS' THEN 6 WHEN 'SEC' THEN 6 WHEN 'MID' THEN 8 WHEN 'MID,INT' THEN 8 WHEN 'MID,HS,AH' THEN 8 WHEN 'MID,AH' THEN 8 WHEN 'HS' THEN 10 WHEN 'HS,AH' THEN 10 ELSE 0 END),IF(gradelevel,gradelevel, CASE loc_cat_subcat WHEN 'ELEM' THEN 5 WHEN 'ELEM,INT,MID' THEN 9 WHEN 'INT' THEN 7 WHEN 'INT,MID,HS' THEN 12 WHEN 'SEC' THEN 12 WHEN 'MID' THEN 9 WHEN 'MID,INT' THEN 9 WHEN 'MID,HS,AH' THEN 12 WHEN 'MID,AH' THEN 12 WHEN 'HS' THEN 12 WHEN 'HS,AH' THEN 12 ELSE 12 END) FROM csip_v3.category WHERE version = 6 AND category_name NOT LIKE 'Other%' AND category_name NOT LIKE 'Specialists');
+INSERT INTO csip_v5.course (course_name,course_category,active,min_grade,max_grade) VALUES ('Science 4',3,1,4,4),('Science 5',3,1,5,5);
 
 UPDATE csip_v5.course SET course_category = 3 WHERE course_name IN ('Geography');
 UPDATE csip_v5.course SET course_category = 4 WHERE course_name LIKE 'CTE%';
