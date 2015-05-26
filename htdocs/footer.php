e7548e677117e546878a23ca3557dc30151bb741
Michael Hafen <michael.hafen@washk12.org>
2015-05-26 10:42:01 -0600

    Adding a link to oldcsip

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -2,8 +2,9 @@
     <div class="clearfix">
         <img src="https://logos.washk12.org/100year.png">
         <div class="uk-align-right" style="font-size: 9pt;">
-		Developed for <strong>Washington County School District</strong> | A joint venture, powered by <img src="<?= $data['_config']['base_url'] ?>images/blackplum_logo_small.png" width="70" alt="BlackPlum Logo"> <i class="uk-icon-plus"></i> <strong>HAFEN</strong>
- &copy; 2015 Jim Black, Cody Plumhof and Michael Hafen.
+		<p>Developed for <strong>Washington County School District</strong> | A joint venture, powered by <img src="<?= $data['_config']['base_url'] ?>images/blackplum_logo_small.png" width="70" alt="BlackPlum Logo"> <i class="uk-icon-plus"></i> <strong>HAFEN</strong>
+ &copy; 2015 Jim Black, Cody Plumhof and Michael Hafen.</p>
+		<p>The previous CSIP's are available at <a href="https://oldcsip.washk12.org/" target="_BLANK">oldcsip.washk12.org</a></p>
         </div>
     </div>
 <br>
