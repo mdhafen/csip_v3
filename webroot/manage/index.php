c395cb45ddfebbabc2d147e2773a9cf48187a186
Michael Hafen <michael.hafen@washk12.org>
2015-06-15 18:06:43 -0600

    remove dead code from manage/index.php

--- webroot/manage/index.php
+++ webroot/manage/index.php
@@ -4,13 +4,7 @@ include_once( '../../lib/security.phpm' );
 include_once( '../../lib/output.phpm' );
 include_once( '../../lib/user.phpm' );
 
-$dbh = db_connect();
-$users = all_users( $dbh );
-$locations = all_locations( $dbh );
-
 $output = array(
-	'users' => $users,
-	'locations' => $locations,
 );
 output( $output, 'manage/index.tmpl' );
 ?>
