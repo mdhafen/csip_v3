5ce26a1e897d0daab6b3603ae8861a8c22f9d7e7
Michael Hafen <michael.hafen@washk12.org>
2015-12-03 12:02:08 -0700

    Change the cfa's so that the form covers the whole tab, and escape quotes

--- htdocs/cfa.php
+++ htdocs/cfa.php
@@ -1,23 +1,23 @@
 <br>
     <ul id="cfa<?= $count ?>" class="uk-nav uk-nav-side uk-nav-parent-icon" data-uk-nav="{multiple:true}">
+    <form method="post" class="uk-form uk-form-horizontal" action="save_answer.php">
+    <input type="hidden" name="csipid" value="<?= $data['csip']['csipid'] ?>">
+    <input type="hidden" name="categoryid" value="<?= $data['categoryid'] ?>">
+    <input type="hidden" name="courseid" value="<?= $data['courseid'] ?>">
+    <input type="hidden" name="part" value="<?= $part ?>">
+    <input type="hidden" name="op" value="SaveAnswer">
     <li id="cfam<?= $count ?>" class="uk-parent" aria-expanded="true">
         <a href="#"><strong>Common Formative Assessment</strong></a>
         <ul class="uk-nav-sub">
             <div class="uk-panel uk-panel-box uk-panel-box-primary">
                 <p><strong>How will WE know if they LEARNED it?</strong></p>
                 <hr>
-                <p>EFFECTIVE TEAMS utilize COMMON FORMATIVE ASSESSMENTS (CFA) to diagnostically assess a student's learning and determine which students were proficient in the guaranteed skill and those who weren't.</p>
+                <p>EFFECTIVE TEAMS utilize COMMON FORMATIVE ASSESSMENTS (CFA) to diagnostically assess a student&quot;s learning and determine which students were proficient in the guaranteed skill and those who weren&quot;t.</p>
                 <p>TEAM ACTION STEPS: Identify a common formative assessment that your team will use to assess the GVC skill.</p>
             </div>
             <br>
 
             <li class="uk-panel uk-panel-box">
-                <form method="post" class="uk-form uk-form-horizontal" action="save_answer.php">
-                    <input type="hidden" name="csipid" value="<?= $data['csip']['csipid'] ?>">
-                    <input type="hidden" name="categoryid" value="<?= $data['categoryid'] ?>">
-                    <input type="hidden" name="courseid" value="<?= $data['courseid'] ?>">
-                    <input type="hidden" name="part" value="<?= $part ?>">
-                    <input type="hidden" name="op" value="SaveAnswer">
                     <div class="uk-badge uk-badge-notification uk-align-right"><?= $count ?></div>
                     <div class="uk-form-row">
                         <label class="uk-form-label" for="cfa-m-<?= $part ?>-15">List the common formative assessment AND the guaranteed skill it aligns with (Example: Unit 1 - Fractions). FOR ELEMENTARY: Identify which questions on your existing instructional program assessments align with your GVC.</label>
@@ -47,7 +47,7 @@
                     </div>
                     <hr>
                     <div class="uk-form-row">
-                        <label class="uk-form-label" for="cfa-m-<?= $part ?>-18">According to the results of this CFA and our team's collaboration, the following teaching practices/strategies were most effective in teaching this guaranteed skill(s):</label>
+                        <label class="uk-form-label" for="cfa-m-<?= $part ?>-18">According to the results of this CFA and our team&quot;s collaboration, the following teaching practices/strategies were most effective in teaching this guaranteed skill(s):</label>
                         <div class="uk-form-controls">
                             <input type="hidden" name="questions[]" value="18">
                             <input type="hidden" name="answerids[]" value="<?= isset($questions[18][0]['answerid']) ? $questions[18][0]['answerid'] : "" ?>">
@@ -58,7 +58,6 @@
   <?php if ( !empty($data['can_edit']) ) { ?>
 					<button class="uk-button uk-button-success uk-align-right" type="button" onclick="save_answers(this)">Save</button>
   <?php } ?>
-                </form>
             </li>
         </ul>
     </li>
@@ -68,20 +67,14 @@
         <ul class="uk-nav-sub">
             <li class="uk-parent">
                 <div class="uk-panel uk-panel-box uk-panel-box-primary">
-                <p><strong>How will WE RESPOND to those who didn't get it?</strong></p>
+                <p><strong>How will WE RESPOND to those who didn&quot;t get it?</strong></p>
 
-                <p>EFFECTIVE TEAMS analyze the results of their common formative assessment (CFA) and immediately intervene with those who are in need of extra time and support. (Keep in mind that if less than 75% of students didn't get a concept, it's not an intervention problem; the initial instruction needs to be examined.)</p>
+                <p>EFFECTIVE TEAMS analyze the results of their common formative assessment (CFA) and immediately intervene with those who are in need of extra time and support. (Keep in mind that if less than 75% of students didn&quot;t get a concept, it&quot;s not an intervention problem; the initial instruction needs to be examined.)</p>
                     </div>
                 <br>
             </li>
 
             <li class="uk-panel uk-panel-box">
-                <form method="post" class="uk-form uk-form-horizontal" action="save_answer.php">
-                    <input type="hidden" name="csipid" value="<?= $data['csip']['csipid'] ?>">
-                    <input type="hidden" name="categoryid" value="<?= $data['categoryid'] ?>">
-                    <input type="hidden" name="courseid" value="<?= $data['courseid'] ?>">
-                    <input type="hidden" name="part" value="<?= $part ?>">
-                    <input type="hidden" name="op" value="SaveAnswer">
                     <div class="uk-badge uk-badge-notification uk-align-right"><?= $count ?></div>
                     <div class="uk-form-row">
                         <label class="uk-form-label" for="cfa-i-<?= $part ?>-21">List the SPECIFIC INTERVENTIONS that your team responded with for those students who WERE NOT proficient.</label>
@@ -93,7 +86,7 @@
                     </div>
                     <hr>
                     <div class="uk-form-row">
-                        <label class="uk-form-label" for="cfa-i-<?= $part ?>-22">Following your team's INTERVENTIONS and REASSESSMENT, how many students are still not proficient?</label>
+                        <label class="uk-form-label" for="cfa-i-<?= $part ?>-22">Following your team&quot;s INTERVENTIONS and REASSESSMENT, how many students are still not proficient?</label>
                         <div class="uk-form-controls">
                             <input type="hidden" name="questions[]" value="22">
                             <input type="hidden" name="answerids[]" value="<?= isset($questions[22][0]['answerid']) ? $questions[22][0]['answerid'] : "" ?>">
@@ -102,7 +95,7 @@
                     </div>
                     <hr>
                     <div class="uk-form-row">
-                        <label class="uk-form-label" for="cfa-i-<?= $part ?>-23">List the SPECIFIC FIRST NAMES of those students who were not proficient even after your team's intervention.<br><br>(To indicate growth, include how much the student grew from the first to second assessment).</label>
+                        <label class="uk-form-label" for="cfa-i-<?= $part ?>-23">List the SPECIFIC FIRST NAMES of those students who were not proficient even after your team&quot;s intervention.<br><br>(To indicate growth, include how much the student grew from the first to second assessment).</label>
 <?php if ( !empty($data['_session']['CAN_view_protected_answers']) ) { ?>
                         <div class="uk-form-controls">
                             <input type="hidden" name="questions[]" value="23">
@@ -127,7 +120,6 @@
   <?php if ( !empty($data['can_edit']) ) { ?>
 					<button class="uk-button uk-button-success uk-align-right" type="button" onclick="save_answers(this)">Save</button>
   <?php } ?>
-                </form>
             </li>
         </ul>
     </li>
@@ -143,13 +135,7 @@
                 </li>
 
             <li class="uk-panel uk-panel-box">
-            <form method="post" class="uk-form uk-form-horizontal" action="save_answer.php">
-                    <input type="hidden" name="csipid" value="<?= $data['csip']['csipid'] ?>">
-                    <input type="hidden" name="categoryid" value="<?= $data['categoryid'] ?>">
-                    <input type="hidden" name="courseid" value="<?= $data['courseid'] ?>">
-                    <input type="hidden" name="part" value="<?= $part ?>">
-                    <input type="hidden" name="op" value="SaveAnswer">
-                        <div class="uk-badge uk-badge-notification uk-align-right"><?= $count ?></div>
+                <div class="uk-badge uk-badge-notification uk-align-right"><?= $count ?></div>
                     <div class="uk-form-row">
                         <label class="uk-form-label" for="cfa-l-<?= $part ?>-27">ACTION STEPS: As you review your GVC from (step #1), identify extension activities your team will use for those who already know it.</label>
                         <div class="uk-form-controls">
@@ -181,11 +167,11 @@
   <?php if ( !empty($data['can_edit']) ) { ?>
 					<button class="uk-button uk-button-success uk-align-right" type="button" onclick="save_answers(this)">Save</button>
   <?php } ?>
-                </form>
             </li>
         </ul>
     </li>
     <br>
+</form>
 </ul>
 
 <hr>
