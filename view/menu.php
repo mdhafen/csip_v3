34ea0ed67e1b702237875d14ccc7e18d69f860d2
Michael Hafen <michael.hafen@washk12.org>
2018-10-05 16:50:02 -0600

    Fix the nav bar drop down on small screens.

--- view/menu.php
+++ view/menu.php
@@ -1,31 +1,49 @@
 <nav class="uk-navbar">
 	<div class="uk-container uk-container-center">
-		<?php
-		if(! empty($data['_session']['username']) ){
-		echo '
-		<div style="float:right; vertical-align:middle;" class="">
-			<ul class="uk-navbar-nav uk-hidden-small uk-navbar-attached">
-				<li class=""><a href="#">Hello, ' . $data['_session']['username'] . '</a></li>
-				<li class=""><a href="'. $data['_config']['base_url'] .'index.php?_logout=1">Logout</a></li>
-			</ul>
-		</div>';
-		}
-		?>
 		<a href="<?= $data['_config']['base_url'] ?>index.php" class="uk-navbar-brand">Digital CSIP <font size="1pt">v. 8.0.bse</font></a>
-		<?php
-		if( !empty($data['_session']['CAN_view_reports']) ){
-                                echo '<ul class="uk-navbar-nav uk-hidden-small uk-navbar-attached">';
-				echo '<li><a href="'. $data['_config']['base_url'] .'reports/index.php">Reports</a></li>
-				</ul>';
-		}
-		if( !empty($data['_session']['CAN_manage_users']) ){
-                                echo '<ul class="uk-navbar-nav uk-hidden-small uk-navbar-attached">';
-				echo '<li><a href="'. $data['_config']['base_url'] .'manage/index.php">Administration</a></li>
-				</ul>';
-		}
-		?>
-		<div class="uk-navbar-flip">
-			<a href="#mainmenu-id" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas="{target:'#mainmenu-id'}"></a>
+		<div class="uk-hidden-small uk-navbar-attached">
+			<?php
+			if(! empty($data['_session']['username']) ){
+			echo '
+			<ul class="uk-navbar-nav uk-navbar-flip">
+				<li><div class="uk-navbar-content">Hello, ' . $data['_session']['username'] . '</div></li>
+				<li><a href="'. $data['_config']['base_url'] .'index.php?_logout=1">Logout</a></li>
+			</ul>';
+			}
+			?>
+
+			<ul class="uk-navbar-nav">
+			<?php
+			if( !empty($data['_session']['CAN_view_reports']) ){
+				echo '<li><a href="'. $data['_config']['base_url'] .'reports/index.php">Reports</a></li>';
+			}
+			if( !empty($data['_session']['CAN_manage_users']) ){
+				echo '<li><a href="'. $data['_config']['base_url'] .'manage/index.php">Administration</a></li>';
+			}
+			?>
+			</ul>
+		</div>
+		<div class="uk-visible-small uk-navbar-flip uk-navbar-attached">
+			<ul class="uk-navbar-nav">
+				<li data-uk-dropdown="{mode:'click'}">
+					<a href="" class="uk-navbar-toggle"></a>
+					<div class="uk-dropdown uk-dropdown-navbar">
+						<ul class="uk-nav uk-nav-navbar">
+						<?php
+						if( !empty($data['_session']['CAN_view_reports']) ){
+							echo '<li><a href="'. $data['_config']['base_url'] .'reports/index.php">Reports</a></li>';
+						}
+						if( !empty($data['_session']['CAN_manage_users']) ){
+							echo '<li><a href="'. $data['_config']['base_url'] .'manage/index.php">Administration</a></li>';
+						}
+						if(! empty($data['_session']['username']) ) {
+							echo '<li><a href="'. $data['_config']['base_url'] .'index.php?_logout=1">Logout</a></li>';
+						}
+						?>
+						</ul>
+					</div>
+				</li>
+			</ul>
 		</div>
 	</div>
 </nav>
